4f346fa6cfc41ac7803fee24fac3a31bf7ec0ee1
[sharoun <[madoyasharoun@gmail.com]>
2023-03-21 12:02:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+// code your solution her
+function saturdayFun (action ) {
+    switch (action) {
+        case "bathe my dog":
+            return "This Saturday, I want to bathe my dog!";
+            break;
+            default:
+        return "This Saturday, I want to roller-skate!";
+    }
+}
+function mondayWork (action){
+    switch (action) {
+        case "work from home":
+            return "This Monday, I will work from home.";
+            break;
+            default:
+        return "This Monday, I will go to the office.";
+    }
+}
+// function wrapAdjective (visualFlair) { 
+//     if (visualFlair = "*")
+//  return function (adjective= "special"){
+//     return `You are ${visualFlair}${adjective} ${visualFlair}`
+//  }
+// }
+
+function wrapAdjective(flair = "*"){
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+ 
